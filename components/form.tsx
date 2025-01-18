0b42d1f4a313b1801919ccbf80b5f826d5edc673
Sricor <78024433+Sricor@users.noreply.github.com>
2025-01-18 13:29:05 +0800

    update

--- components/form.tsx
+++ components/form.tsx
@@ -10,9 +10,41 @@ import {
   Card,
   CardBody,
 } from "@nextui-org/react";
+import { useRouter } from "next/router";
+import { client } from "@/library/http/client";
 
 export const SignForm = () => {
   const [selected, setSelected] = React.useState<string>("login");
+  const [name, setName] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(client.api);
+    alert((await client.health.ping()).timestamp);
+  };
+
+  const handleSignUp = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // try {
+    //   const response = await fetch("/api/signup", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ name, password }),
+    //   });
+    //   const data = await response.json();
+    //   if (response.ok) {
+    //     localStorage.setItem("jwt", data.token);
+    //     router.push("/dashboard");
+    //   } else {
+    //     alert(data.message);
+    //   }
+    // } catch (error) {
+    //   console.error("Sign up failed:", error);
+    // }
+  };
 
   return (
     <div className="flex items-center justify-center">
@@ -26,18 +58,20 @@ export const SignForm = () => {
             onSelectionChange={setSelected as (key: React.Key) => void}
           >
             <Tab key="login" title="Login">
-              <form className="flex flex-col gap-4">
+              <form className="flex flex-col gap-4" onSubmit={handleLogin}>
                 <Input
                   isRequired
                   label="NAME"
                   placeholder="Enter your name"
                   type="name"
+                  onChange={(e) => setName(e.target.value)}
                 />
                 <Input
                   isRequired
                   label="PASSWORD"
                   placeholder="Enter your password"
                   type="password"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
                 <p className="text-center text-small">
                   Need to create an account?{" "}
@@ -46,31 +80,37 @@ export const SignForm = () => {
                   </Link>
                 </p>
                 <div className="flex gap-2 justify-end">
-                  <Button fullWidth color="primary">
+                  <Button fullWidth color="primary" type="submit">
                     Login
                   </Button>
                 </div>
               </form>
             </Tab>
             <Tab key="sign-up" title="Sign up">
-              <form className="flex flex-col gap-4 h-[300px]">
+              <form
+                className="flex flex-col gap-4 h-[300px]"
+                onSubmit={handleSignUp}
+              >
                 <Input
                   isRequired
                   label="NAME"
                   placeholder="Enter your name"
-                  type="password"
+                  type="name"
+                  onChange={(e) => setName(e.target.value)}
                 />
                 <Input
                   isRequired
                   label="NAME"
                   placeholder="Enter your name"
                   type="name"
+                  onChange={(e) => setName(e.target.value)}
                 />
                 <Input
                   isRequired
                   label="PASSWORD"
                   placeholder="Enter your password"
                   type="password"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
                 <p className="text-center text-small">
                   Already have an account?{" "}
@@ -79,7 +119,7 @@ export const SignForm = () => {
                   </Link>
                 </p>
                 <div className="flex gap-2 justify-end">
-                  <Button fullWidth color="primary">
+                  <Button fullWidth color="primary" type="submit">
                     Sign up
                   </Button>
                 </div>
