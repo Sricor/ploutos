89a8736f1c184a08a605c16c26e7c88c9c500ef9
Sricor <78024433+Sricor@users.noreply.github.com>
2025-01-18 09:13:39 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
 
 export const metadata: Metadata = {
   title: {
@@ -48,7 +47,6 @@ export default function RootLayout({
 
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,13 @@
 "use client";
 
-import { Tabs, Tab } from "@nextui-org/react";
+import { Tabs, Tab, Button } from "@nextui-org/react";
 import { MusicIcon, HeartFilledIcon, SunIcon, MoonIcon } from "./icons";
 import React from "react";
 
-
 export const Navbar = () => {
   const color = "primary" as const;
-  const [isDarkMode, setIsDarkMode] = React.useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-    // 这里可以添加切换主题的逻辑，比如修改全局状态或调用主题切换的 API
-    document.body.classList.toggle("dark", !isDarkMode);
-  };
 
   return (
-    <div className="bg-background shadow-lg rounded-t-lg">
       <div className="mx-auto p-2">
         <Tabs key="navbar" color={color} radius="full">
           <Tab key="photos" title="Photos" />
@@ -26,16 +17,9 @@ export const Navbar = () => {
           <Tab key="person" title="Person" />
           <Tab
             key="theme"
-            title={
-              <div className="flex items-center space-x-2">
-                {isDarkMode ? <MoonIcon /> : <SunIcon />}
-                <span>{isDarkMode ? "Dark Mode" : "Light Mode"}</span>
-              </div>
-            }
-            onClick={toggleTheme} // 自定义点击事件
+            title={<MoonIcon/>}
           />
         </Tabs>
       </div>
-    </div>
   );
 };
